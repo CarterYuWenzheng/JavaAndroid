a174bfa4273e7d04e642561866736e0eff25292c
Carter_于 <carteryuwenzheng@163.com>
2019-06-15 18:23:00 +0800

    增加utils

---
+++ app/src/main/java/com/carter/javaAndroid/utils/GlideImageLoader.java
@@ -0,0 +1,26 @@
+package com.carter.javaAndroid.utils;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.carter.javaAndroid.R;
+
+public class GlideImageLoader {
+    /**
+     *
+     * @param context context
+     * @param url image url
+     * @param iv imageView
+     */
+    public static void load(Context context, String url, ImageView iv) {
+        GlideApp.with(context)
+                .load(url)
+                .placeholder(R.drawable.bg_placeholder)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .diskCacheStrategy(DiskCacheStrategy.DATA)
+                .into(iv);
+
+    }
+}
